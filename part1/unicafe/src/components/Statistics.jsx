8b7b30175760b1d76314500ddfa6646f7ea5e0d5
Aitor <aitordediego23@gmail.com>
2025-04-17 12:04:54 +0200

    Exercise 1.9

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,4 +1,12 @@
 const Statistics = (props) => {
+  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
+    return (
+      <>
+        <h2>No feedback given</h2>
+      </>
+    )
+  }
+
   return (
     <div className="statistics-container">
       <h2>statistics</h2>
