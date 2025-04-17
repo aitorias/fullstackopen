4b884b3bda6898ed3e4927d03b588c24cbb31495
Aitor <aitordediego23@gmail.com>
2025-04-17 12:41:15 +0200

    Exercise 1.11 refactor code

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,11 +1,11 @@
 import StatisticLine from './StatisticLine'
 
-const Statistics = (props) => {
-  const all = props.good + props.neutral + props.bad
-  const average = all === 0 ? 0 : (props.good - props.bad) / all
-  const positive = all === 0 ? 0 : (props.good * 100) / all
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = all === 0 ? 0 : (good - bad) / all
+  const positive = all === 0 ? 0 : (good * 100) / all
 
-  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
+  if (all === 0) {
     return (
       <>
         <h2>statistics</h2>
@@ -19,9 +19,9 @@ const Statistics = (props) => {
       <h2>statistics</h2>
       <table>
         <tbody>
-          <StatisticLine text="good" value={props.good} />
-          <StatisticLine text="neutral" value={props.neutral} />
-          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
           <StatisticLine text="all" value={all} />
           <StatisticLine text="average" value={average} />
           <StatisticLine text="positive" value={positive + ' %'} />
